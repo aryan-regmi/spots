21760e2c63f0acedde563e8cdaaaad7347738deb
Aryan Regmi <aryan_regmi@protonmail.com>
2025-08-25 11:06:35 -0600

    Added mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,28 @@ import { PlaylistPage } from './pages/PlaylistPage';
 import { SignupPage } from './pages/SignupPage';
 import { getAuthUser } from './services/api/database';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { getCurrentWindow, LogicalSize } from '@tauri-apps/api/window';
+import { useEffect } from 'react';
 
+// TODO: TEST ON ANDROID!!!
+//
 // TODO: Add doc comments to all public stuff at lease
-//
-// TODO: Sanitize username and password for SQL (extra characters etc)
-//
 //
 // TODO: Add a export/backup option so that everything can be backed up
 
 /** The main component of the application. */
 function App() {
+    // FIXME: Remove in prod
+    //
+    // Sets window to mobile view
+    let window = getCurrentWindow();
+    useEffect(() => {
+        async function mobileView() {
+            await window.setSize(new LogicalSize(375, 812));
+        }
+        mobileView();
+    }, [window]);
+
     // Setup query client
     const queryClient = new QueryClient();
 
