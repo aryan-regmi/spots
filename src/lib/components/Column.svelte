90c80f3b3299526db7ac9fe0566d1b60ff73141f
Aryan Regmi <aryan_regmi@protonmail.com>
2025-09-14 22:20:37 -0600

    fixed column

---
+++ src/lib/components/Column.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    const {
+        spacing = '0.1em',
+        class: className = '',
+        children,
+        style = '',
+        ...restProps
+    } = $props();
+</script>
+
+<div class="col {className}" style="gap: {spacing}; {style}" {...restProps}>
+    {@render children()}
+</div>
+
+<style>
+    .col {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/lib/pages/login/LoginPage.svelte
+++ src/lib/pages/login/LoginPage.svelte
@@ -1,34 +1,70 @@
 <script lang="ts">
+    import Column from '@/components/Column.svelte';
+    import Button from '@smui/button';
     import Textfield from '@smui/textfield';
     import HelperText from '@smui/textfield/helper-text';
 
-    let usernameInput = $state({ username: '', isValid: false });
+    let username = $state('');
+    let password = $state('');
+
+    let usernameInput = $derived({ username, isValid: true });
+    let passwordInput = $derived({ password, isValid: true });
 </script>
 
-<!-- <div class="col"> -->
-<div>
+<Column spacing="0.5em" class="login-form">
+    <h1 class="app-title">Spots</h1>
+
     <Textfield
+        class="login-page-input"
         variant="filled"
-        bind:value={usernameInput.username}
+        bind:value={username}
         invalid={!usernameInput.isValid}
         label="Username"
+        required
     >
         {#snippet helper()}
             <HelperText>Enter username</HelperText>
         {/snippet}
     </Textfield>
-</div>
+
+    <Textfield
+        class="login-page-input"
+        variant="filled"
+        bind:value={password}
+        invalid={!passwordInput.isValid}
+        label="Password"
+        type="password"
+        required
+    >
+        {#snippet helper()}
+            <HelperText>Enter password</HelperText>
+        {/snippet}
+    </Textfield>
+
+    <Button>Login</Button>
+</Column>
 
 <style>
-    .row {
-        display: flex;
-        flex-direction: row;
-        gap: 1em;
+    .app-title {
+        margin-bottom: 3em;
+        margin-top: -1em;
+        padding-top: 0;
     }
 
-    .col {
-        display: flex;
-        flex-direction: col;
-        gap: 1em;
+    :global(.login-page-input.mdc-text-field--focused) {
+        background-color: rgba(50, 50, 50, 1);
+    }
+
+    :global(.login-page-input .mdc-text-field__ripple) {
+        display: none;
+    }
+
+    :global(.login-page-input) {
+        border-radius: '0.1em';
+    }
+
+    :global(.login-form) {
+        justify-content: center;
+        align-items: center;
     }
 </style>
