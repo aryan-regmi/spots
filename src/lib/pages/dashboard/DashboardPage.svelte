d93d735350d3bf33840b5b269d6eb89425c8d99a
Aryan Regmi <aryan_regmi@protonmail.com>
2025-09-22 11:45:47 -0600

    added doc comments

--- src/lib/pages/dashboard/DashboardPage.svelte
+++ src/lib/pages/dashboard/DashboardPage.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
-  import { closeEndpoint } from '@/api/network';
-  import type { AuthContext } from '@/auth/types';
-  import type { NavContext } from '@/router/types';
-  import { getContext } from 'svelte';
+    import { closeEndpoint } from '@/api/network';
+    import type { AuthContext } from '@/auth/types';
+    import Column from '@/components/Column.svelte';
+    import type { NavContext } from '@/router/types';
+    import { Button } from 'bits-ui';
+    import { getContext } from 'svelte';
 
-  const { unauthorize } = getContext<AuthContext>('authContext');
-  const { navigateTo } = getContext<NavContext>('navContext');
+    const { unauthorize } = getContext<AuthContext>('authContext');
+    const { navigateTo } = getContext<NavContext>('navContext');
 
-  let isLoggingOut = $state(false);
+    /** Determines if currently in the process of running out. */
+    let isLoggingOut = $state(false);
+
+    /** Unauthenticates the user, closes the network endpoint and returns to the landing page. */
+    async function logout() {
+        isLoggingOut = true;
+        await unauthorize();
+        await closeEndpoint();
+        await navigateTo('/', { replace: true });
+    }
 </script>
 
-<div>
-  Dashboard!
-
-  <button
-    onclick={async () => {
-      isLoggingOut = true;
-      await unauthorize();
-      await closeEndpoint();
-      navigateTo('/', { replace: true });
-    }}
-  >
-    {#if isLoggingOut}
-      Logging out...
-    {:else}
-      Log out
-    {/if}
-  </button>
-</div>
+<Column>
+    <Button.Root onclick={logout}>
+        {#if isLoggingOut}
+            Logging out...
+        {:else}
+            Log out
+        {/if}
+    </Button.Root>
+</Column>
