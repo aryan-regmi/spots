42d32f8528913381b2470d793ad64afd53f6c1fb
Aryan Regmi <aryan_regmi@protonmail.com>
2025-09-23 21:16:47 -0600

    updated alert style

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -1,72 +1,82 @@
 <script lang="ts">
-    import Card from './Card.svelte';
-    import Row from './Row.svelte';
-    import WarningCircle from 'phosphor-svelte/lib/WarningCircle';
+  import { getContext } from 'svelte';
+  import Card from './Card.svelte';
+  import Row from './Row.svelte';
+  import WarningCircle from 'phosphor-svelte/lib/WarningCircle';
+  import type { ThemeContext } from '@/theme/types';
+  import { themeContextKey } from '@/theme/themeContextKey';
 
-    type Props = {
-        class?: string;
-        style?: string;
-        level?: 'basic' | 'warning' | 'error';
-        children?: any;
-    };
-    const {
-        class: className = '',
-        style = '',
-        level = 'basic',
-        children,
-    }: Props = $props();
+  type Props = {
+    class?: string;
+    style?: string;
+    level?: 'basic' | 'warning' | 'error';
+    children?: any;
+  };
+  const {
+    class: className = '',
+    style = '',
+    level = 'basic',
+    children,
+  }: Props = $props();
 
-    const cardStyles = $state({
-        margin: 0,
-        padding: '0.5em 1.5em',
-        justifyContent: 'center',
-        backgroundColor: '',
-        color: '',
-        border: '1px ridge white',
-        borderColor: '',
-    });
+  const { currentPalette } = getContext<ThemeContext>(themeContextKey);
+  const palette = $derived(currentPalette());
 
-    // Updates the styles based on the alert level.
-    $effect(() => {
-        switch (level) {
-            case 'basic':
-                cardStyles.backgroundColor = 'rgba(255, 255, 255, 0.5)';
-                cardStyles.color = 'rgba(200, 200, 200, 1)';
-                cardStyles.borderColor = 'rgba(255, 226, 183, 0.8)';
-            case 'warning':
-                cardStyles.backgroundColor = 'rgba(110, 110, 30, 0.3)';
-                cardStyles.color = 'rgba(255, 255, 190, 1)';
-                cardStyles.borderColor = 'rgba(255, 226, 183, 1)';
-            case 'error':
-                cardStyles.backgroundColor = 'rgba(110, 50, 50, 0.3)';
-                cardStyles.color = 'rgba(255, 100, 100, 1)';
-                cardStyles.borderColor = 'rgba(255, 100, 100, 1)';
-            default:
-                break;
-        }
-    });
+  const cardStyles = $state({
+    margin: 0,
+    padding: '0.5em 1.5em',
+    justifyContent: 'center',
+    backgroundColor: '',
+    color: '',
+    border: '1px ridge white',
+    borderColor: '',
+  });
+
+  // Updates the styles based on the alert level.
+  $effect(() => {
+    switch (level) {
+      case 'basic':
+        cardStyles.backgroundColor = palette.background.surface;
+        cardStyles.color = palette.text.primary;
+        cardStyles.borderColor = palette.border.strong;
+        break;
+      case 'warning':
+        console.log('here');
+        cardStyles.backgroundColor = palette.text.secondary;
+        cardStyles.color = palette.text.inverted;
+        cardStyles.borderColor = palette.accent.main;
+        break;
+      case 'error':
+        cardStyles.backgroundColor = palette.basic.primary;
+        cardStyles.color = palette.text.inverted;
+        cardStyles.borderColor = palette.border.light;
+        break;
+      default:
+        break;
+    }
+  });
 </script>
 
 <Card
-    class={className}
-    {style}
-    --card-margin={cardStyles.margin}
-    --card-padding={cardStyles.padding}
-    --card-justify-content={cardStyles.justifyContent}
-    --card-background-color={cardStyles.backgroundColor}
-    --card-color={cardStyles.color}
-    --card-border={cardStyles.border}
-    --card-border-color={cardStyles.borderColor}
+  class={className}
+  {style}
+  --card-margin={cardStyles.margin}
+  --card-padding={cardStyles.padding}
+  --card-justify-content={cardStyles.justifyContent}
+  --card-background-color={cardStyles.backgroundColor}
+  --card-color={cardStyles.color}
+  --card-border={cardStyles.border}
+  --card-border-color={cardStyles.borderColor}
 >
-    <Row
-        spacing="0.5em"
-        --row-margin={0}
-        --row-padding={0}
-        --row-justify-content={'center'}
-        --row-align-items={'center'}
-        --row-vertical-align={'middle'}
-    >
-        <WarningCircle style="font-size: 2em" />
-        {@render children()}
-    </Row>
+  <Row
+    spacing="0.5em"
+    --row-margin={0}
+    --row-padding={0}
+    --row-justify-content={'center'}
+    --row-align-items={'center'}
+    --row-vertical-align={'middle'}
+  >
+    <WarningCircle style="font-size: 2em" />
+    {@render children()}
+  </Row>
 </Card>
