bc2ff84f7404193d5dde293ff6e709b148a99fa9
Aryan Regmi <aryan_regmi@protonmail.com>
2026-02-15 20:44:30 -0700

    Added Shimmer

--- src/pages/NavMusicLayout.tsx
+++ src/pages/NavMusicLayout.tsx
@@ -1,9 +1,18 @@
+import { useAuth } from '@/services/auth/provider';
+import { createAsync } from '@solidjs/router';
+import { Shimmer } from '@shimmer-from-structure/solid';
+
 export function NavMusicLayout(props: { children?: any }) {
+  const auth = useAuth();
+  const getAuthUserQuery = createAsync(() => auth.getAuthUserQuery());
+
   return (
     <>
-      <div>Hello</div>
-      {props.children}
-      <div>Goodbye</div>
+      <Shimmer loading={getAuthUserQuery() === undefined}>
+        <div>Hello</div>
+        {props.children}
+        <div>Goodbye</div>
+      </Shimmer>
     </>
   );
 }
