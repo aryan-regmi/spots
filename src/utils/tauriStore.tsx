44165e575646886772adfd97577b30f89a9307a3
Aryan Regmi <aryan_regmi@protonmail.com>
2026-02-18 22:14:33 -0700

    Added tauri store (as cookie alternative)

--- src/utils/tauriStore.tsx
+++ src/utils/tauriStore.tsx
@@ -1,22 +1,22 @@
 import { load, Store } from '@tauri-apps/plugin-store';
-import { ResultAsync } from 'neverthrow';
+import { err, ok, ResultAsync } from 'neverthrow';
 import { createContext, createResource, Resource, useContext } from 'solid-js';
 import { Logger } from './logger';
+import { createError } from './errors';
 
 /** The path of the store. */
-const STORE_PATH = 'spots-store.json';
+export const STORE_PATH = 'spots-store.json';
 
-/** Represents an error in `Store` operations. */
-export type StoreError = {
-  kind?: string;
-  message: string;
-  stack?: string;
-};
+/** Errors returned by the store. */
+export type StoreError =
+  | { InvalidStore: 'Store must be initalized' }
+  | { OpenStoreError: 'Unable to open the store' }
+  | { AddEntryError: 'Unable to add entry to the store' };
 
-/** The things */
-export type StoreServices = {
+/** The actual context. */
+export type StoreInnerContext = {
   store: Resource<Store | undefined>;
-  setValue: <T>(
+  addEntry: <T>(
     store: Store,
     entry: {
       key: string;
@@ -27,12 +27,13 @@ export type StoreServices = {
     store: Store,
     key: string
   ) => ResultAsync<T | undefined, StoreError>;
+  removeEntry: (store: Store, key: string) => ResultAsync<boolean, StoreError>;
   saveStore: (store: Store) => ResultAsync<void, StoreError>;
   closeStore: (store: Store) => ResultAsync<void, StoreError>;
 };
 
 /** The `Store` context. */
-export const StoreContext = createContext<StoreServices>();
+export const StoreContext = createContext<StoreInnerContext>();
 
 /** Provides the `Store` context. */
 export function StoreProvider(props: { children: any }) {
@@ -49,8 +50,9 @@ export function StoreProvider(props: { children: any }) {
   /** Create services for the provider. */
   const services = {
     store,
-    setValue,
+    addEntry,
     getValue,
+    removeEntry,
     saveStore,
     closeStore,
   };
@@ -78,12 +80,19 @@ function openStore() {
         reject(e as Error);
       }
     }),
-    makeStoreError
+    (e) =>
+      createError(
+        { OpenStoreError: 'Unable to open the store' },
+        {
+          store: STORE_PATH,
+          error: e,
+        }
+      )
   );
 }
 
 /** Sets a value in the store. */
-function setValue<T>(store: Store, entry: { key: string; value: T }) {
+function addEntry<T>(store: Store, entry: { key: string; value: T }) {
   return ResultAsync.fromPromise(
     new Promise<void>((resolve, reject) => {
       try {
@@ -92,7 +101,11 @@ function setValue<T>(store: Store, entry: { key: string; value: T }) {
         reject(e as Error);
       }
     }),
-    makeStoreError
+    (e) =>
+      createError(
+        { AddEntryError: 'Unable to add entry to the store' },
+        { store, key: entry.key, error: e }
+      )
   );
 }
 
@@ -102,6 +115,24 @@ function getValue<T>(store: Store, key: string) {
     new Promise<T | undefined>((resolve, reject) => {
       try {
         store.get<T>(key).then(resolve);
+      } catch (e) {
+        reject(e as Error);
+      }
+    }),
+    makeStoreError
+  );
+}
+
+/**
+ * Removes the entry with the given key from the store.
+ *
+ * Returns `true` if there was an entry to delete, else `false`.
+ * */
+function removeEntry(store: Store, key: string) {
+  return ResultAsync.fromPromise(
+    new Promise<boolean>((resolve, reject) => {
+      try {
+        store.delete(key).then(resolve);
       } catch (e) {
         reject(e as Error);
       }
@@ -136,14 +167,4 @@ function closeStore(store: Store) {
     }),
     makeStoreError
   );
-}
-
-/** Converts an `Error` to `StoreError` */
-function makeStoreError(error: unknown) {
-  const err = error as Error;
-  return {
-    kind: err.name,
-    message: err.message,
-    stack: err.stack,
-  } as StoreError;
 }
